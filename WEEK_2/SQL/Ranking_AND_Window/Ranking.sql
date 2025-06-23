36f30a181b9797fe9dc9ffd8c90e9ef1ea5d178e
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2025-06-24 00:18:34 +0530

    Create Ranking.sql

---
+++ WEEK_2/SQL/Ranking_AND_Window/Ranking.sql
@@ -0,0 +1,18 @@
+--Exercise 1: Ranking and Window Functions
+--Goal: Use ROW_NUMBER(), RANK(), DENSE_RANK(), OVER(), and PARTITION BY.
+--Scenario:
+--Find the top 3 most expensive products in each category using different ranking functions.
+--Steps:
+--1. Use ROW_NUMBER() to assign a unique rank within each category.
+--2. Use RANK() and DENSE_RANK() to compare how ties are handled.
+--3. Use PARTITION BY Category and ORDER BY Price DESC.
+sqlcmd -S localhost
+SELECT
+    RIGHT('     ' + CAST(P_ID AS VARCHAR), 5) AS P_ID,
+    LEFT(P_NAME + SPACE(15), 15) AS P_NAME,
+    LEFT(CATEGORY + SPACE(15), 15) AS CATEGORY,
+    RIGHT('       ' + CAST(PRICE AS VARCHAR), 10) AS PRICE,
+    RIGHT('     ' + CAST(ROW_NUMBER() OVER (PARTITION BY CATEGORY ORDER BY PRICE DESC) AS VARCHAR), 5) AS P_RANK
+FROM Products;
+GO
+
