31357769349b5bd6b83c4ec7cc9f2c3a04108fcf
Dipanwita Sen <130593147+DipanwitSen@users.noreply.github.com>
2025-06-24 21:44:38 +0530

    Update FUNCTION.sql

--- WEEK_2/SQL/STORED_PROCEDURE/FUNCTION.sql
+++ WEEK_2/SQL/STORED_PROCEDURE/FUNCTION.sql
@@ -1,3 +1,8 @@
+--Exercise 7: Return Data from a Scalar Function 
+--Goal: Return the annual salary for a specific employee using `fn_CalculateAnnualSalary`. 
+--Steps: 
+--1. Execute the `fn_CalculateAnnualSalary` function for an employee with `EmployeeID = 1`. 
+--2. Verify the result.
 CREATE FUNCTION CalculateAnnualSal (@Salary DECIMAL(10,2))
 RETURNS DECIMAL(10,2)
 AS
